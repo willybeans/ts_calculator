7f9c99aa9f09baf99d5dd29ec0a244d7898f72d3
willybeans <willwedmedyk@gmail.com>
2018-02-07 13:50:58 -0500

    adding button functionality

--- calculator.js
+++ calculator.js
@@ -1,32 +1,79 @@
 var buttons = ['0','1','2','3','4','5','6',
-               '7','8','9','=','+','-','C','.'];
-var text = [];
+               '7','8','9','=','+','-','CE','AC','.'];
+var currentEntry = [], totalEntry = [];
+var testNum = /[0-9]/g;
 
 window.onload = () => {
-  inputField();
+ // inputField();
   makeButtons();
-  applyClick();
+  //applyClick();
 }
 
 function applyClick(x) {
   var btn = document.getElementById("b" + x);
-  var textArea = document.getElementById("textArea");
-  btn.onclick = () => {
-    if(textArea.textContet === numbers) {
-    textArea.textContent = (""+x+ textArea.textContent);
-    } else
-  }
-}
+  var totalArea = document.getElementById("totalArea");
+  var currentArea = document.getElementById("currentArea");
 
-function inputField() {
-  var field = document.createElement("div");
-  var text = document.createTextNode(" ");
-  field.id = "textArea";
-  field.style.background = "black";
-  field.style.color = "white";
-  field.appendChild(text);
-  document.body.appendChild(field);
-}
+  btn.onclick = () => {
+    if(!isNaN(x)) { //test for number
+      if(currentEntry[0] === undefined){ //clear for first entry
+        currentArea.textContent = " ";
+        totalArea.textContent = " ";
+      }
+      if(isNaN(currentEntry[0])){ //clear after using operands
+        currentArea.textContent = " ";
+        currentEntry = [];
+      }
+     currentEntry.push(x);
+     currentArea.textContent = (currentArea.textContent + x);
+     totalArea.textContent = (totalArea.textContent + x);
+  } else {
+    switch (x) {
+      case "CE": //clear Entry only
+        currentEntry = [];
+        textArea.textContent = "0";
+        break;
+      case "AC": //Clear All *
+        currentEntry = [];
+        totalEntry = [];
+        totalArea.textContent = "0";
+        currentArea.textContent = "0"
+        break;
+      case "+":
+        currentArea.textContent = x;
+        totalArea.textContent = (totalArea.textContent + x);
+        currentEntry = ["+"];
+        totalEntry.push(x);
+        break;
+      case "-":
+        textArea.textContent = (totalArea.textContent + x);
+        break;
+      case ".":
+        textArea.textContent = (totalArea.textContent + x);
+        break;
+      case "=":
+        totalEntry.push(totalArea.textContent);
+        // console.log(totalArea.textContent)
+        // console.log(currentArea.textContent);
+        //console.log(text);
+        break;
+    }
+   }
+    //console.log(totalEntry);
+    console.log(currentEntry);
+  }
+ }
+
+// function inputField() {
+//   var field = document.createElement("div");
+//   var text = document.createTextNode("0");
+//   field.id = "textArea";
+//   field.style.background = "black";
+//   field.style.color = "white";
+//   field.appendChild(text);
+//   document.body.appendChild(field);
+// }
+//consider using this for the subarray
 
 function makeButtons() {
   for (var i = 0; i < buttons.length; i++){
@@ -40,3 +87,7 @@ function makeButtons() {
 
   }
 }
+
+/* should we have a var set to a string that can populate our form
+
+*/
