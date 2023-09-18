a2c9a41d25faca00a98eae4118b16b4ccff761cc
Will Wedmedyk <willwedmedyk@gmail.com>
2023-09-18 19:32:10 -0400

    ww/fix helpers logic + add test file

--- src/tsModules/helpers.ts
+++ src/tsModules/helpers.ts
@@ -3,32 +3,8 @@ export function getText(query: string): string {
   return (el && el.textContent) || "";
 }
 
-export function returnLastEntry(userInput: string): string {
-  const currentArea = getText("currentArea");
-  return currentArea.split("").pop() || "";
-}
-
-export function filterUserInput(userInput: string): string[] {
-  const regexOperands = /[+\-\/x=]/;
-  const currentArea = getText("currentArea");
-  let testCurrentEntry: string[] = [];
-  switch (userInput) {
-    case ".":
-      testCurrentEntry = currentArea.split(regexOperands);
-      break;
-    case "=":
-      // testCurrentEntry = currentArea; //.split(regexOperands)
-      testCurrentEntry = currentArea.split(/([+\-\/x=])/g);
-      break;
-    case "CE":
-      testCurrentEntry = currentArea.split("");
-      testCurrentEntry.pop();
-      break;
-    default:
-      break;
-  }
-  return testCurrentEntry;
-}
+export const returnLastEntry = (): string =>
+  getText("currentArea").split("").pop() || "";
 
 export function changeDisplay(userInput: string) {
   const faceHappy = document.getElementById("face-happy") as HTMLElement;
